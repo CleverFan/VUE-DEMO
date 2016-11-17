ab6ba5e0d8886a75b9d9156598b9d699eae0b827
CleverFan <377109151@qq.com>
2016-11-17 18:21:01 +0800

    update index page

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,33 @@ router.get('/LeaveMsg',function (req,res,next) {
   res.render('LeaveMsg-easy/app')
 });
 
+router.get('/home',function (req,res,next) {
+  res.writeHead(302,{
+    'Location':'http://wwww.improvecfan.cn'
+  });
+  res.end();
+});
+
+router.get('/goany',function (req,res,next) {
+  var url = [
+    'http://www.baidu.com',
+      'http://www.improvecfan.cn',
+      'http://blog.improvecfan.cn',
+      'https://github.com',
+      'https://github.com/CleverFan',
+      'http://blog.csdn.net/qq_31655965',
+      'https://www.google.com.hk/?gws_rd=ssl',
+      'http://www.htmleaf.com/',
+      'https://www.zhihu.com',
+      'http://www.runoob.com/nodejs/nodejs-tutorial.html'
+  ]
+  var randomNum = parseInt(10*Math.random());
+  res.writeHead(302,{
+    'Location': url[randomNum]
+  });
+  res.end();
+});
+
 
 
 module.exports = router;
